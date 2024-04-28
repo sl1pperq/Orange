1b1b862b14320dfa248f46f4237982a2e2bdaced
Sergey Averin <averin_sd@icloud.com>
2024-04-28 14:03:29 +0300

    -

--- controllers/games.js
+++ controllers/games.js
@@ -1,18 +1,12 @@
-const {readData} = require("../utils/data");
-const sendAllGames = async (req, res, next) => {
-    const games = await readData("./data/games.json");
-    if (!games) {
-        res.status(400);
-        res.send({
-            status: "error",
-            message: "Нет игр в базе данных. Добавьте игру."
-        });
-        return;
-    }
-    req.games = games;
-    next()
+const sendAllGames = (req, res) => {
+    res.send(req.games);
 };
 
-module.exports = {
-    sendAllGames
-}
+const sendUpdatedGames = (req, res) => {
+    res.send({
+        games: req.games,
+        updated: req.updatedObject
+    });
+};
+
+module.exports = { sendAllGames, sendUpdatedGames };
